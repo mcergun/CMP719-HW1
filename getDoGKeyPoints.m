08909e2da864671104e673884c5a0a5c64fda913
Mert Can Ergün <mcergun@windowslive.com>
2016-11-13 20:10:27 +0300

    Added difference of gaussians keypoint detector script

---
+++ getDoGKeyPoints.m
@@ -0,0 +1,16 @@
+function [out] = getDoGKeyPoints(img, sigma1, fsize, thr)
+if ~mod(fsize, 2)
+    error('filter size should be odd');
+end
+
+imsize = size(img);
+
+dog = fspecial('gaussian', fsize, sigma1) - fspecial('gaussian', fsize, sigma1 + 5);
+out = zeros(imsize);
+for n = 1:imsize(3)
+out(:, :, n) = imfilter(img(:, :, n), dog);
+end
+% out(out < thr) = 0;
+% out(out >= thr) = 1;
+imshow(out);
+end
